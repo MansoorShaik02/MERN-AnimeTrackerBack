8f21666edbbd6320f9193b91cf3b613848540919
mansoor <mansoorshaik7b@gmail.com>
2024-11-12 16:25:03 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ connectDB();
 app.get("/", (req, res) => {
     res.json("Hello")
 })
+app.use((req, res, next) => {
+    res.header(
+        "Access-Control-Allow-Origin",
+        "React app URL"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 app.use(cors(
 
     {
