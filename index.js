95b9b288802342aa4d5e8227e16e99a82f316b02
mansoor <mansoorshaik7b@gmail.com>
2024-11-12 21:48:29 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,40 +4,28 @@ const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 //require('dotenv').config('{path:D:/reactproectsreal/MERNAnimeDB/MERN-AnimeTracker/backend/.env}');
 
-
 const app = express();
 
 // Connect to database
 connectDB();
 
 // Middleware
+app.use(cors({
+    origin: 'https://mern-anime-tracker-front.vercel.app', // Frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
+
+app.use(express.json());
+
+// Test route
 app.get("/", (req, res) => {
-    res.json("Hello")
-})
-app.use((req, res, next) => {
-    res.header(
-        "Access-Control-Allow-Origin",
-        "React app URL"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
+    res.json("Hello");
 });
 
-app.use(cors(
-
-    {
-        origin: ["https://mern-anime-tracker-front.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-
-    }
-));
-app.use(express.json());
-//haha
 // Routes
-
 app.use('/api/users', userRoutes);
 
+// Start server
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
