11dcda432e07de440f5310f509efc142c67931b3
mansoor <mansoorshaik7b@gmail.com>
2024-11-15 21:46:29 +0530

    port

--- index.js
+++ index.js
@@ -82,4 +82,4 @@ app.use('/api/users', userRoutes);
 
 // Start server
 const PORT = 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
