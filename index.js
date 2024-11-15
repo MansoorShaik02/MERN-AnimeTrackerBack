7f0d7074fc7fce2bfcbd01d29608349f2c2dbcb6
mansoor <mansoorshaik7b@gmail.com>
2024-11-15 13:49:33 +0530

    changes made to index.js

--- index.js
+++ index.js
@@ -2,33 +2,15 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
-//require('dotenv').config('{path:D:/reactproectsreal/MERNAnimeDB/MERN-AnimeTracker/backend/.env}');
+require('dotenv').config({ path: 'D:/reactprojectsreal/MERNAnimeDB/MERN-AnimeTracker/backend/.env' });
 
 const app = express();
 
 // Connect to database
 connectDB();
 
-app.use((req, res, next) => {
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "https://mern-anime-tracker-front.vercel.app"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-    res.setHeader("Access-Control-Max-Age", 7200);
-
-    next();
-});
+//you can mimic * by getting origin from: const origin = req.headers.origin;
+//and passing origin in, instead of a static hostname
 
 // Middleware
 app.use(cors({
@@ -48,5 +30,5 @@ app.get("/", (req, res) => {
 app.use('/api/users', userRoutes);
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
